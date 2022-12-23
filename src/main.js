502c68565922e9bb13c57596aa654b775e01a6fd
Dmitry Lazenyuk <lazenyukdima@gmail.com>
2022-12-23 15:50:34 +0600

    add bootstrap

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-import './assets/main.css'
+import "~bootstrap/scss/bootstrap.scss";
+import "@/styles/base.scss";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
+app.use(router);
 
-app.mount('#app')
+app.mount("#app");
